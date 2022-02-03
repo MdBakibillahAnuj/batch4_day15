71e3da07207ed98a9d27992b1af59a889f9fc4f6
MdBakibillahAnuj <mdbakibillah789@gmail.com>
2022-02-03 17:24:05 +0600

    project php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'vendor/autoload.php';
+use \App\classes\Exlample;
+
+$example = new Exlample();
+$example->index();
+
+/*
+ 3 major rules for variable
+        *start with $ sign
+        * A-Z ,a-z,0-9,-()
+        * no numbers in first
+      **standard rules for variable Name
+        *start with small letter
+        *meaningful
+        *readable
+ *
+ */
